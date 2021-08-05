3c785ee90d91dfd9868d0e635c086df1173fb2de
madeleineernst <enmeilin@gmail.com>
2021-08-05 12:48:31 +0200

    fix to issue #7

--- R/qc_general.R
+++ R/qc_general.R
@@ -323,7 +323,7 @@ plot_compound_sd_vs_mean <- function(data,
   # Add label in low density areas
   if (!is.null(label)) {
     g <- g +
-      ggpmisc::stat_dens2d_filter(
+      ggpp::stat_dens2d_filter(
         mapping = aes_string(label = paste0("`", label, "`"),
                              color = "Sample.Type"),
         geom = ggrepel::geom_text_repel()$geom, keep.number = label_number)
